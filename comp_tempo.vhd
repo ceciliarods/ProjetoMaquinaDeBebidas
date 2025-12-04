e1f0b71fd025499c396c2c8dfbbcc504208ba032
Cecília Rodrigues Saraiva <84354894+ceciliarods@users.noreply.github.com>
2025-12-04 16:41:07 -0300

    Fix comment for output signal in comp_tempo

--- comp_tempo.vhd
+++ comp_tempo.vhd
@@ -10,7 +10,7 @@ use IEEE.std_logic_1164.all;
 entity comp_tempo is
     port(
         tempo   : in  std_logic_vector(5 downto 0);
-        saida   : out std_logic  -- igual a 1 quando o contador = 011110
+        saida   : out std_logic  -- igual a 1 quando o contador = 11110
     );
 end entity;
 
